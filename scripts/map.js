707e19d1af0ea87c36ecf710801abefda1cef8e3
Joshy <jmealing91@hotmail.com>
2018-05-05 23:41:16 +1000

    maps -made functions to get positions of objects and append objects to DOM, styles -added styles for objects

--- scripts/map.js
+++ scripts/map.js
@@ -1,20 +1,47 @@
 
 const $backgroundImg = document.querySelector('.gameImg');
-
+const $gameScreen = document.querySelector('.game-screen');
+let currentScene = 0;
 const scenes = [
     {
-        imgSrc : 'images/image.jpg'
+        imgSrc : 'images/image.jpg',
+        objects : [
+            {   props : {
+                startX : 396,
+                startY : 229,
+                width : 182,
+                height : 86 },            
+                type : 'table'
+            },
+            {   props : {
+                startX : 687,
+                startY : 236,
+                width : 104,
+                height : 121 },            
+                type : 'chair'
+            }
+        ]
     },
     {},
     {}
 ];
 
 window.addEventListener('load', () => {
-    $backgroundImg.src = scenes[0].imgSrc;
+    $backgroundImg.src = scenes[currentScene].imgSrc;
+    // $backgroundImg.style.backgroundImage = `url(${scenes[currentScene].imgSrc})`;
 });
 
 // make function to get the mouse coordinates so I know where to place interactable objects
-let clicks = 0;
+let clickCount = 0;
+
+let elemDims = {
+    start : {
+        x : '',
+        y : ''
+    },
+    width : '',
+    height : ''
+};
 
 let clickCoords = [];
 // clickCoords[0] first click top left
@@ -27,26 +54,53 @@ $backgroundImg.addEventListener('click', getCoords);
 
 function getCoords(e) {
     // e.preventDefault();
-
-    if (clicks > 3) {
-        clicks = 3;
-        // run function to work out start position and height and width of interactable item
-        return false;
-    } else {
-        let startX = e.clientX;
-        let startY = e.clientY;
+    if (clickCount < 3) { // runs first block up until 4 clicks on screen
+        // gets positions of all clicks and pushes to array
         let arrayIndex = {
             x : e.clientX,
             y : e.clientY
         };
         clickCoords.push(arrayIndex);
-        clicks++;
-
+        clickCount++; // adds +1 to click count
+    } else { // after four clicks have happened
+        // code to work out starting position coords, height and width of element
+        elemDims.start.x = clickCoords[0].x;
+        elemDims.start.y = clickCoords[0].y;
+        elemDims.width = clickCoords[1].x - clickCoords[0].x;
+        elemDims.height = clickCoords[2].y - clickCoords[1].y;
+        console.table(elemDims);
     }
-
-    console.log(clickCoords);
-    
 }
+
+// function to push the interactable elements to DOM
+scenes[currentScene].objects.forEach(obj => {
+    let index = [...scenes[currentScene].objects].indexOf(obj);
+    let node = document.createElement('article');
+    $gameScreen.appendChild(node);
+
+    let objects = $gameScreen.querySelectorAll('article');
+
+    objects[index].classList.add('object');
+    objects[index].setAttribute('data-object', scenes[currentScene].objects[index].type);
+    objects[index].style.top = `${scenes[currentScene].objects[index].props['startY']}px`;
+    objects[index].style.left = `${scenes[currentScene].objects[index].props['startX']}px`;
+    objects[index].style.height = `${scenes[currentScene].objects[index].props['height']}px`;
+    objects[index].style.width = `${scenes[currentScene].objects[index].props['width']}px`;
+
+    console.log(objects);
+
+
+});
+
+function renderObjects() {
+
+// var node = document.createElement("LI");                 // Create a <li> node
+// var textnode = document.createTextNode("Water");         // Create a text node
+// node.appendChild(textnode);                              // Append the text to <li>
+// document.getElementById("myList").appendChild(node);     // Append <li> to <ul> with id="myList"
+
+}
+
 
 
 
