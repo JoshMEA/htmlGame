aa77ff739f58a70086d7a8b54787b602f44b3b61
Joshy <jmealing91@hotmail.com>
2018-05-05 20:31:17 +1000

    made the add and subtract functions cleaner, as well as pulling the values from charStats object as opposed to the DOM elements

--- characterCreate.js
+++ characterCreate.js
@@ -1,28 +1,18 @@
-const menuCont = document.querySelector('.menu-container');
-const charCreate = menuCont.querySelector('.char-create');
-const statsList = charCreate.querySelector('.stats-list');
-const attr = statsList.querySelectorAll('span');
-const skillPoints = charCreate.querySelector('.skill-points');
-const subBtn = charCreate.querySelectorAll('[data-sub]');
-const addBtn = charCreate.querySelectorAll('[data-add]');
-const attrVal = statsList.querySelectorAll('[data-attrVal]');
-const attrName = statsList.querySelectorAll('[data-attrName');
-const createBtn = charCreate.querySelector('button');
+const $menuCont = document.querySelector('.menu-container');
+const $charCreate = $menuCont.querySelector('.char-create');
+const $statsList = $charCreate.querySelector('.stats-list');
+const $attribute = $statsList.querySelectorAll('span');
+const $skillPoints = $charCreate.querySelector('.skill-points');
+const $subBtn = $charCreate.querySelectorAll('[data-sub]');
+const $addBtn = $charCreate.querySelectorAll('[data-add]');
+const $attrVal = $statsList.querySelectorAll('[data-attrVal]');
+const $attrName = $statsList.querySelectorAll('[data-attrName]');
+const $createBtn = $charCreate.querySelector('button');
 
-window.addEventListener('load',() => {
-    skillPoints.textContent = charStats.skillPoints;
-
-    for (let i = 0; i < attrName.length; i++) {
-        let attr = attrName[i].dataset;
-        attrVal.textContent = charStats.attr;
-        console.log();
-
-    }
-});
 
 let charStats = {
     level : 1,
-    skillPoints : 50,
+    skillPoints : 10,
     name : '',
     HP : 100,
     MP : 50,
@@ -33,40 +23,53 @@ let charStats = {
     speed : 0
 };
 
+window.addEventListener('load',() => {
+    $skillPoints.textContent = charStats['skillPoints'];
 
+    for (let i = 0; i < $attrName.length; i++) {
+        let attr = $attrName[i].dataset.attrname;
+        $attrVal[i].textContent = charStats[attr];
+        console.log(charStats.attr);
 
-subBtn.forEach(btn => btn.addEventListener('click', () => {
-    let index = [...subBtn].indexOf(btn);
-    //gets last p element of each attribute line
-    let val = attr[index].querySelectorAll('p')[2];
-    // console.log(val);
-    if (val.textContent <= 0) {
-        return false;
-    } else {
-        val.textContent--;
-        charStats.skillPoints++;
-        console.log(charStats.skillPoints);
-    }    
-    skillPoints.textContent++;
-}));
-
-addBtn.forEach(btn => btn.addEventListener('click', () => {
-    let index = [...addBtn].indexOf(btn);
-    //gets last p element of each attribute line
-    let val = attr[index].querySelectorAll('p')[2];
-
-    if (skillPoints.textContent <= 0) {
-        return false;
-    } else {
-        val.textContent++;
-        charStats.skillPoints--;
-        console.log(charStats.skillPoints);
-        skillPoints.textContent--;   
     }
-}));
+
+});
 
 
-createBtn.addEventListener('click', () => {
+$addBtn.forEach(btn => btn.addEventListener('click', addAttribute));
+$subBtn.forEach(btn => btn.addEventListener('click', subAttribute));
+
+function addAttribute() {
+    const index = [...$addBtn].indexOf(this);
+    const currentAttribute = $attrName[index].dataset.attrname; // gets the attribute that is being added to
+
+    if (charStats['skillPoints'] > 0) { // if player has skill points
+        charStats[currentAttribute]++; // + 1 to that attribute in the charStats object
+        charStats['skillPoints']--; // -1 from the skill point bank in charStats object
+        $skillPoints.textContent = charStats['skillPoints']; // updates the amount of skill points available in the DOM
+        const currentAttributeVal = charStats[currentAttribute]; //gets the current attributes value
+        $attrVal[index].textContent = currentAttributeVal; // updates the amount of points for that attribute in the DOM
+        console.log(currentAttribute, charStats[currentAttribute]);
+
+    }
+}
+
+function subAttribute() {
+    const index = [...$subBtn].indexOf(this);
+    const currentAttribute = $attrName[index].dataset.attrname; // gets the attribute that is being subtracted
+
+    if (charStats[currentAttribute] <= 0) {
+            return false;
+        } else {
+            charStats[currentAttribute]--; // + 1 to that attribute in the charStats object
+            charStats['skillPoints']++; // +1 to the skill point bank in charStats object
+            $skillPoints.textContent = charStats['skillPoints']; // updates the amount of skill points available in the DOM
+            const currentAttributeVal = charStats[currentAttribute]; //gets the current attributes value
+            $attrVal[index].textContent = currentAttributeVal; // updates the amount of points for that attribute in the DOM
+        }
+}
+
+$createBtn.addEventListener('click', () => {
     charStats.name = charCreate.querySelector('input').value;
     console.log(charStats.name);
 });
